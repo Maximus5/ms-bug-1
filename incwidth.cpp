3ed4479757284b56411aa92a980f9774265eed06
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-06-12 19:59:08 +0300

    Show result of API calls

--- incwidth.cpp
+++ incwidth.cpp
@@ -9,14 +9,14 @@ void main()
 	CONSOLE_CURSOR_INFO ci = {};
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	GetConsoleScreenBufferInfo(hOut, &csbi);
-	GetConsoleCursorInfo(hOut, &ci);
-	printf("Cursor height before=%i%%\r\n", ci.dwSize);
+	BOOL lb1 = GetConsoleCursorInfo(hOut, &ci);
+	printf("Cursor height before=%i%% (rc=%u)\r\n", ci.dwSize, lb1);
 	SetConsoleTitle(_T("Increasing ScreenBufferSize"));
 	Sleep(2500);
 	csbi.dwSize.X++;
 	SetConsoleScreenBufferSize(hOut, csbi.dwSize);
 	SetConsoleTitle(_T("Quering current cursor information"));
 	Sleep(2500);
-	GetConsoleCursorInfo(hOut, &ci);
-	printf("Cursor height after=%i%%\r\n", ci.dwSize);
+	BOOL lb2 = GetConsoleCursorInfo(hOut, &ci);
+	printf("Cursor height after=%i%% (rc=%u)\r\n", ci.dwSize, lb2);
 }

cb1d483ca185e52c45b249c7575e74b122b01869
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-06-12 19:23:27 +0300

    Initial version

---
+++ incwidth.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <windows.h>
+
+void main()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi = {};
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hOut, &csbi);
+	csbi.dwSize.X++;
+	SetConsoleScreenBufferSize(hOut, csbi.dwSize);
+}

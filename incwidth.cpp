b5dd8f63e0d0421c6c2772aa224cc5abfa826954
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-06-12 19:36:11 +0300

    Print cursor size before and after

--- incwidth.cpp
+++ incwidth.cpp
@@ -1,11 +1,17 @@
 ﻿
 #include <windows.h>
+#include <stdio.h>
 
 void main()
 {
 	CONSOLE_SCREEN_BUFFER_INFO csbi = {};
+	CONSOLE_CURSOR_INFO ci = {};
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	GetConsoleScreenBufferInfo(hOut, &csbi);
+	GetConsoleCursorInfo(hOut, &ci);
+	printf("Cursor height before=%i%%\n", ci.dwSize);
 	csbi.dwSize.X++;
 	SetConsoleScreenBufferSize(hOut, csbi.dwSize);
+	GetConsoleCursorInfo(hOut, &ci);
+	printf("Cursor height after=%i%%\n", ci.dwSize);
 }
